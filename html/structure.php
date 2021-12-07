0206e2997b07100c204498485491fd276d92965a
Hyeonjeong <76260153+rgl-za@users.noreply.github.com>
2021-12-07 14:28:48 +0900

    Update structure.php

---
+++ html/structure.php
@@ -0,0 +1,35 @@
+<!--주석수정-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>이지팜 - EasyFarm Monitoring</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">    
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'header.php'?>
+    
+  
+
+    <?php include 'footer.php'?>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/jquery-1.7.2.min.js"></script>
+	
+    <script src="js/bootstrap.js"></script>
+    <script src="js/base.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
