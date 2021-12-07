47dca5e5ef9a499d9e637949bc6a645023a1711d
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-12-07 14:31:01 +0900

    Update footer.php

---
+++ html/footer.php
@@ -0,0 +1,21 @@
+<div class="footer">
+  <div class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+		<div class="copy_right_info">
+          COMPANY : (주)이지팜  /  OWNER : 이미진, 지현정  /  CALL CENTER : 1234-1324<br>
+          ADDRESS : 부산광역시 부산진구 엄광로 176 동의대학교<br>
+          개인정보관리책임자 : 이미진, 지현정<br>
+          사업자등록번호 : [231-54-4325] /  통신판매업 신고번호  : 제1234-2341호[사업자정보확인]
+          </div>
+		&copy; 2021 <a href="#">이지팜 easyfarm</a>. 
+		</div>
+        <!-- /span12 --> 
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /footer-inner --> 
+</div>
